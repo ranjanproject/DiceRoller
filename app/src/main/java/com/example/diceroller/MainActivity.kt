4e63c96c49dc143f1a6ab28bfef6ac71414528a4
Pratik Ranjan <pratikranjaniitg101@gmail.com>
2022-06-22 15:38:21 +0530

    Roll function added

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
     }
+
+    private fun init(){
+        initializeButton()
+    }
+
+    private fun initializeButton(){
+        val btn = findViewById<Button>(R.id.roll_btn)
+
+        btn.setOnClickListener {
+            showRolledNumber()
+        }
+    }
+
+    private fun showRolledNumber(){
+        val num = getRandomNumber()
+        findViewById<TextView>(R.id.num_text).text = num.toString()
+    }
+
+    private fun getRandomNumber(): Int{
+        return (1..6).random()
+    }
+
 }
