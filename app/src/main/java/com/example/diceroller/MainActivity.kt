afff54b8c168b7302e48f5a7bcdefd81f6c9b978
Pratik Ranjan <pratikranjaniitg101@gmail.com>
2022-06-22 16:57:20 +0530

    dice image added

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,17 +23,30 @@ class MainActivity : AppCompatActivity() {
         val btn = findViewById<Button>(R.id.roll_btn)
 
         btn.setOnClickListener {
+//            Toast.makeText(this, "Amount can not be  grater than invoice", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
             showRolledNumber()
         }
     }
 
     private fun showRolledNumber(){
-        val num = getRandomNumber()
-        findViewById<TextView>(R.id.num_text).text = num.toString()
+
+        setDiceImage(getRandomNumber())
     }
 
     private fun getRandomNumber(): Int{
         return (1..6).random()
+    }
+
+    private fun setDiceImage(num: Int){
+        when(num){
+            1 -> findViewById<ImageView>(R.id.dice_image).setImageResource(R.drawable.dice_1)
+            2 -> findViewById<ImageView>(R.id.dice_image).setImageResource(R.drawable.dice_2)
+            3 -> findViewById<ImageView>(R.id.dice_image).setImageResource(R.drawable.dice_3)
+            4 -> findViewById<ImageView>(R.id.dice_image).setImageResource(R.drawable.dice_4)
+            5 -> findViewById<ImageView>(R.id.dice_image).setImageResource(R.drawable.dice_5)
+            6 -> findViewById<ImageView>(R.id.dice_image).setImageResource(R.drawable.dice_6)
+        }
     }
 
 }
